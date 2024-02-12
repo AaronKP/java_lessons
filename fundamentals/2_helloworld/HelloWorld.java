828d919ae907391755d1b79185e8002332fa0de6
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-12 23:42:47 +0200

    Variables 2.1.1

--- fundamentals/2_helloworld/HelloWorld.java
+++ fundamentals/2_helloworld/HelloWorld.java
@@ -3,3 +3,38 @@ public class HelloWorld {
 		System.out.println("Hello World");
 	}
 }
+
+public class Employee{
+	public static void main(String [] args){
+		System.out.println("Hello World");
+	}
+}
+
+public class Student{
+	public static void main(String [] args){
+		System.out.println("Hello World");
+	}
+}
+
+public class Country{
+	public static void main(String [] args){
+		System.out.println("Hello World");
+	}
+}
+
+public class Animal {
+	public static void main(String [] args){
+		System.out.println("Hello World");
+	}
+}
+
+public class Food{
+	public static void main(String [] args){
+		System.out.println("Hello World");
+	}
+}
+public class Book{
+	public static void main(String [] args){
+		System.out.println("Hello World");
+	}
+}
---
+++ fundamentals/3_variables/mycode/VariableTest.java
@@ -0,0 +1,6 @@
+public class VariableTest{
+    public static void main(String [] args){
+        int myNumber = 7;
+        System.out.println("Variable contains :"+myNumber);
+    }
+}
