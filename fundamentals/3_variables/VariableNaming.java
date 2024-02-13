86cd5c885f1101ef9b3b29bb7d8df05078a0d2de
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-13 09:58:26 +0200

    Lesson variable naming rules complete

---
+++ fundamentals/3_variables/VariableNaming.java
@@ -0,0 +1,11 @@
+public class VariableNaming {
+	public static void main (String [] args){
+
+		String employeeName = "Yamcha";
+		final double POWER_LEVEL = 1;
+
+		System.out.println("Employee name: "+ employeeName) ;
+		System.out.println("Power level: "+ POWER_LEVEL) ;
+	}
+
+}
