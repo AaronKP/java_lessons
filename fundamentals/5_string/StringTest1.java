a52989800c024ca7de4551bd09abd1e232ba36d8
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-13 14:45:53 +0200

    string concatenation

---
+++ fundamentals/5_string/StringTest1.java
@@ -0,0 +1,9 @@
+public class StringTest1{
+    public static void main(String [] args){
+        String name = "Aaron";
+        String surname = "Padiachy";
+		String fullName = name+" "+surname;
+
+        System.out.println("Full Name :"+fullName);
+    }
+}
