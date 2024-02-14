e8a8aebdd9e50d7a3e2f525b5ca58e5aaab74abc
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-14 16:27:07 +0200

    increment/decrement lesson

---
+++ fundamentals/12_incdec/Aaron/Increment3.java
@@ -0,0 +1,8 @@
+public class Increment3 {
+    public static void main(String[] args) {
+ 	   int myInt = 2;
+ 	   System.out.println("Before operation myInt = "+myInt);//2
+ 	   System.out.println("During operation myInt = "+ (myInt--));//2
+ 	   System.out.println("After operation myInt = "+myInt);//1 		   
+    }
+}
