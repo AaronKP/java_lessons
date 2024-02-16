870ee2c595e916ef8726cd32dce90a4519e209ac
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-16 17:52:10 +0200

    switches

---
+++ fundamentals/21_switch/SwitchTest.java
@@ -0,0 +1,22 @@
+public class SwitchTest {
+    public static void main(String[] args) {
+        int a = 4;
+
+        switch(a){
+            case 1: {
+                 System.out.println("Cool 1");
+            }break;
+            case 2:{
+                System.out.println("Cool 2");
+            }break;
+            case 3: {
+                System.out.println("Cool 3");
+            }break;
+            default :{
+                System.out.println("Not cool");
+            }break;
+
+        }
+
+    }
+}
