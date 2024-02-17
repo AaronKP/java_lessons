ecb240dbe331dab3d0a80e8c3fb50d248f3adb72
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-17 15:50:46 +0200

    arithmetic promotion

---
+++ fundamentals/18_promo/Aaron/Promote1.java
@@ -0,0 +1,8 @@
+public class Promote1{
+    public static void main(String[] args) {
+        double a = 5;
+        short b = 2;
+        double c = a +b;     
+        System.out.println(c);
+    }
+}
