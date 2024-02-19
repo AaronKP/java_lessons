ca56eca9154df3e1206734f93dca3180c4c674c4
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-19 11:11:45 +0200

    for loops lesson

---
+++ fundamentals/17_casting/Aaron/Cast1.java
@@ -0,0 +1,10 @@
+public class Cast1{
+    public static void main(String[] args) {
+        float a = 5F;
+        long b = (long)a;
+
+     
+        System.out.println(b);
+    }
+}
+
---
+++ fundamentals/22_for/Aaron/ForTest1.java
@@ -0,0 +1,12 @@
+public class ForTest1 {
+    public static void main(String[] args) {
+        for(int i = 0;i <10;i++){
+            if(i%2!=0){
+                System.out.println("Counter = "+i);
+            }
+            
+        }
+         
+    }
+}
+
