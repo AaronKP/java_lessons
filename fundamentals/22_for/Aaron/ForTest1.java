01ddad306e26c0a0369e1cadedeb225a820299e3
AaronKP <aaronkpadiachy98@gmail.com>
2024-02-20 07:13:54 +0200

    for loops

--- fundamentals/22_for/Aaron/ForTest1.java
+++ fundamentals/22_for/Aaron/ForTest1.java
@@ -1,12 +1,14 @@
 public class ForTest1 {
     public static void main(String[] args) {
-        for(int i = 0;i <10;i++){
+       for(int i = 0;i <10;i++){
             if(i%2!=0){
-                System.out.println("Counter = "+i);
+                System.out.println("Counter = "+i+ "numeric type int");
             }
             
         }
-         
     }
 }
+
+
+
 
