1a6f136d52338de9d3beec5a649256e29d44d9f1
AaronKP <aaronkpadiachy98@gmail.com>
2024-05-27 15:26:38 +0200

    garbage collection

--- OOP/28_gc/Aaron/Garbage.java
+++ OOP/28_gc/Aaron/Garbage.java
@@ -1,11 +1,16 @@
 public class Garbage{
-	public String name;
+	private String name;
 
-	public Garbage(String paramName){
-		this.name = paramName;
+	public String getName(){
+		return name;
 	}
 
-	public void finalize(){
-		System.out.println("Finalizing just before garbage collection");
+	public Garbage(String paramName){
+		name=paramName;
+	}
+
+	@Override
+	protected void finalize(){
+		System.out.println("Finalizing before garbage collection");
 	}
 }
--- OOP/28_gc/Aaron/GarbageMain.java
+++ OOP/28_gc/Aaron/GarbageMain.java
@@ -1,5 +1,3 @@
-
-
 public class GarbageMain{
 	public static void main(String[] args) {
 		Garbage gc1 = new Garbage("Garbage1");
@@ -7,6 +5,14 @@ public class GarbageMain{
 		Garbage gc3 = new Garbage("Garbage3");
 		Garbage gc4 = new Garbage("Garbage4");
 		Garbage gc5 = new Garbage("Garbage5");
+
+		System.gc();
+
+		Garbage gc6 = new Garbage("Garbage1");
+		Garbage gc7 = new Garbage("Garbage2");
+		Garbage gc8 = new Garbage("Garbage3");
+		Garbage gc9 = new Garbage("Garbage4");
+		Garbage gc10 = new Garbage("Garbage5");
 
 		System.gc();
 	}
